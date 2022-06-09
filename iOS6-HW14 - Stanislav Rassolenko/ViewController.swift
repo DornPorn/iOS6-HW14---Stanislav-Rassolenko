ea859d94bbec2a93da99fdc7a7320a26834fa827
DornPorn <gungan722121@gmail.com>
2022-06-09 13:27:45 +0700

    Initial Commit

---
+++ iOS6-HW14 - Stanislav Rassolenko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-HW14 - Stanislav Rassolenko
+//
+//  Created by Stanislav Rassolenko on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
