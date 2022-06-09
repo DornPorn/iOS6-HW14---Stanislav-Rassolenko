9208fdd2e9f04f9c1fc71fdd34a8e86a2398186f
DornPorn <gungan722121@gmail.com>
2022-06-09 21:39:25 +0700

    - Implemented first collection view section

---
+++ iOS6-HW14 - Stanislav Rassolenko/Model/AlbumCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumCellModel.swift
+//  iOS6-HW14 - Stanislav Rassolenko
+//
+//  Created by Stanislav Rassolenko on 6/9/22.
+//
+
+import Foundation
+
+struct AlbumCellModel {
+    let imageName: String
+    let albumName: String
+    let numberOfPhotos: String
+}
